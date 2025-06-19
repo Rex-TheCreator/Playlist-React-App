2a9f0d5c83ccba1c5d0c92cbb69450128de949f2
Jim Daou <j_d_daou@yahoo.com>
2025-06-19 17:46:32 -0400

    added URI passthrough for tracks

--- Playlist-React-App/src/App.jsx
+++ Playlist-React-App/src/App.jsx
@@ -8,20 +8,28 @@ import Playlist from './components/playlist/playlist';
 
 function App() {
   const [trackData, setTrackData] = useState([
-    { title: "FEiN", author: "Travis Scott", image: svg },
-    { title: "Sigma Boy", author: "Russian Lady", image: svg },
-    { title: "Corazon", author: "Maluma", image: svg }
+    { title: "FEiN", author: "Travis Scott", image: svg , uri:"spotify:track:3KkXRkHbMCARz0aVfEt68P"},
+    { title: "Sigma Boy", author: "Russian Lady", image: svg , uri:"spotify:track:3gHkLiHbMCARz0aVfEt68P"},
+    { title: "Corazon", author: "Maluma", image: svg , uri:"spotify:track:3KkXRkoPmSdDD0aVfEt68P"}
   ]);
   const [playlistTracks, setPlaylistTracks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [playlistName, setPlaylistName] = useState("My Playlist");
+  const [playlistName, setPlaylistName] = useState("Playlist Name");
 
   function search() {
     alert("Searching for " + searchTerm);
     //setPlaylistTracks(trackData.filter(track => track.title.includes(searchTerm)));
   }
   function save() {
-    alert("Saving playlist: " + playlistName);
+    if (playlistTracks.length > 0) {
+      alert(`Saving playlist: ${playlistName} \nTracks Saved: ` + playlistTracks.map(track => track.title).join(", "));
+      alert(`Ready to send the following URIs to Spotify: \n` + playlistTracks.map(track => track.uri).join(", \n"));
+      alert(`Playlist ${playlistName} has saved successfully! Playlist will be reset.`);
+      setPlaylistTracks([]);
+      setPlaylistName("Playlist Name");
+    } else {
+      alert("Playlist is empty. Add some tracks before saving.");
+    }
   }
   function addTrack(track) {
     if (playlistTracks.find(addedTrack => addedTrack.title === track.title)) {
